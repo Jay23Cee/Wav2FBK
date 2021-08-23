274b99d401c793423072d3395ca087ff05c0bef7
jay23cee <jayceexxiii@gmail.com>
2021-08-22 18:47:17 -0700

    Getting data from Wave App GraphQL

--- src/component/wave.js
+++ src/component/wave.js
@@ -1,63 +1,122 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React, { useEffect, useState } from "react";
+import { render } from "@testing-library/react";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   useQuery,
-  gql
+  gql,
+  createHttpLink,
 } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
 
-
-const EXCHANGE_RATES = gql`
-query GetExchangeRates {
-  rates(currency: "USD") {
-    currency
-    rate
+const Query_bussiness = gql`
+  query {
+    businesses {
+      edges {
+        node {
+          id
+          name
+          countries
+        }
+      }
+    }
   }
-}
 `;
 
+function Wave() {
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <h2>WAVE Information Below 🚀</h2>
+
+       <WaveData/>
+      </div>
+    </ApolloProvider>
+  );
+}
+
+const Query_bus = gql`
+  query {
+    user {
+      id
+      defaultEmail
+      firstName
+    }
+    accountTypes {
+      name
+    }
+  }
+  `;
+  
+
+  function WaveData(){
+   
+
+    const { loading, error, data } = useQuery(Query_bus);
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :( {error.message}</p>;
+        console.log(data.user)
+      return  (  
+        <div>
+          <h1> WAVE APPS DATA</h1>
+          <ul>
+            
+              <li key={data.user.id}> {data.user.firstName}</li>
+          
+          </ul>
+
+        </div>
+
+      )
+     
+      
+    
+  }
+
+
+const httpLink = createHttpLink({
+  uri: 'https://gql.waveapps.com/graphql/public',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = 'eyuuMOuBJRYjpf59rFX0euH4cVlBzo';
+  
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
 const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
-function Wave() {
-    
 
-    return (
-      <div>
-        <h2>My first Apollo app 🚀</h2>
-        <ExchangeRates/>
+
+
+
+
+     // let bear_token = "Bearer eyuuMOuBJRYjpf59rFX0euH4cVlBzo";
+    // fetch("https://gql.waveapps.com/graphql/public", {
+    //   method: "POST",
+    //   headers: {
+    //     Authorization: bear_token,
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     query:
+    //       "query { user { id defaultEmail firstName  } accountTypes{name} }",
+    //     variables: {},
+    //   }),
+    // })
+    //   .then((r) => r.json())
+    //   .then((data) =>{ 
+    //   });
       
-      </div>
-    );
-  }
-
-
-  
- 
-
-
-
-
-function ExchangeRates() {
-
-    const { loading, error, data } = useQuery(EXCHANGE_RATES);
-  
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error :(</p>;
-  
-    return data.rates.map(({ currency, rate }) => (
-      <div key={currency}>
-        <p>
-          {currency}: {rate}
-        </p>
-      </div>
-    ));
-  }
-  
-
-
-  export default Wave;
+export default Wave;
