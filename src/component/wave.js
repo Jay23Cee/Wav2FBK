c6426e1ef31df3ed78a34f54cb6bd90be02fe2e8
JAYCEE <Jayceexxiii@gmail.com>
2022-01-09 19:30:11 -0800

    wave apps

--- src/component/wave.js
+++ src/component/wave.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { render } from "@testing-library/react";
 import {
   ApolloClient,
   InMemoryCache,
@@ -9,6 +8,8 @@ import {
   createHttpLink,
 } from "@apollo/client";
 import { setContext } from '@apollo/client/link/context';
+
+const token = 'G5qNRPdeVZMk1AAQIYuEuAGgu729yG';
 
 function Wave() {
   return (
@@ -63,7 +64,6 @@ const httpLink = createHttpLink({
 
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = 'eyuuMOuBJRYjpf59rFX0euH4cVlBzo';
   
   // return the headers to the context so httpLink can read them
   return {
@@ -74,10 +74,76 @@ const authLink = setContext((_, { headers }) => {
   }
 });
 
+
+const shema = `
+mutation ($input: CustomerCreateInput!) {
+  customerCreate(input: $input) {
+    didSucceed
+    inputErrors {
+      code
+      message
+      path
+    }
+    customer {
+      id
+      name
+      firstName
+      lastName
+      email
+      address {
+        addressLine1
+        addressLine2
+        city
+        province {
+          code
+          name
+        }
+        country {
+          code
+          name
+        }
+        postalCode
+      }
+      currency {
+        code
+      }
+    }
+  }
+}` 
+
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
-      
+
+
+fetch('https://gql.waveapps.com/graphql/public', {
+  method: 'POST',
+  headers: {
+    'Authorization': token ? `Bearer ${token}` : "",
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    query: shema,
+    variables: { 
+      "input": {
+      "businessId": "QnVzaW5lc3M6MmU0YTUwMzQtYTI1Mi00ODY4LThkMTYtN2QzNDBjYmE0NzZi",
+      "name": "Donda",
+      "firstName": "Donda",
+      "lastName": "Nias",
+      "email": "dona@example.com",
+      "address": {
+        "city": "North WEST",
+        "postalCode": "H0H 0H0",
+        "provinceCode": "CA-NU",
+        "countryCode": "CA"
+      },
+      "currency": "CAD"
+    }}
+  })
+})
+.then(r => r.json())
+.then(data => console.log(data));
+
 export default Wave;
